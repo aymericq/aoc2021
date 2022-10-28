2b05cb78bcb1f923e29b43e81040e28258bad2c3
Aymeric Quesne <aymeric.quesne@octo.com>
2022-10-28 23:24:40 +0200

    ca avance

--- d18/main3.py
+++ d18/main3.py
@@ -1,3 +1,18 @@
+def main():
+    with open("test_input", 'r') as fd:
+        lines = [line.strip() for line in fd.readlines()]
+    arr0 = eval(lines[0])
+    dm = arr2depthmap(arr0)
+    for i in range(len(lines)):
+        dm1 = arr2depthmap(eval(lines[i]))
+        dm = add(dm, dm1)
+    print(dm)
+    t = depthmap2btree(dm)
+    print_tree(t)
+    s = pretty_print_t(t)
+    print(s)
+
+
 def arr2depthmap(a):
     if isinstance(a, int):
         return {'val': a, 'd': 0}
@@ -32,102 +47,69 @@ def find_end_of_snail_number_after_position(dm, i):
     return None
 
 
-# def depthmap2arr(dm, d=1) -> list:
-#     dmax = max([e['d'] for e in dm])
-#     if d == dmax:
-#         return [dm[0]['val'], dm[1]['val']]
-#     if dm[0]['d'] == d:
-#         # Cas d'un nombre simple à la racine
-#         if dm[1]['d'] == d:
-#             return [dm[0]['val'], dm[1]['val']]
-#         else:
-#             return [dm[0]['val'], depthmap2arr(dm[1:], d=d + 1)]
-#     elif dm[-1]['d'] == d:
-#         return [depthmap2arr(dm[0:-1], d=d + 1), dm[-1]['val']]
-#     else:
-#         i = find_first_regular_pair(dm)
-#         j = find_end_of_snail_number_after_position(dm, i)
-#         return [depthmap2arr(dm[0:j], d=d + 1), depthmap2arr(dm[j:], d=d + 1)]
-
-
-# def depthmap2arr(dm):
-#     i = find_first_regular_pair(dm)
-#     if i - 1 > 0 and dm[i - 1]['d'] == dm[i]['d'] - 1:
-#         arr = [dm[i - 1]['val'], [dm[i]['val'], dm[i + 1]['val']]]
-#         # remonter le fil vers la gauche
-#         j = i - 2
-#         while j >= 0:
-#             arr = [dm[j]['val'], arr]
-#             j -= 1
-#         return arr
-#     elif i + 2 < len(dm) - 1 and dm[i + 2]['d'] == dm[i]['d'] - 1:
-#         arr = [[dm[i]['val'], dm[i + 1]['val']], dm[i + 2]['val']]
-#         while j := find_first_regular_pair(dm[i + 3:]) is not None:
-#             j += i + 3
-#             pass
-#         # Remonter le fil vers la droite
-#         j = i + 3
-#         while j < len(dm):
-#             arr = [arr, dm[j]['val']]
-#             j += 1
-#         return arr
-#     else:
-#         arr = [[dm[i], dm[i + 1]], [dm[i + 2], dm[i + 3]]]
-
-# def depthmap2arr(dm):
-#     d = dm[0]['d']
-#     s = "[" * dm[0]['d'] + str(dm[0]['val'])
-#     i = 1
-#     while i < len(dm):
-#         if dm[i]['d'] > d:
-#             s += "," + "[" * (dm[i]['d'] - d) + str(dm[i]['val'])
-#         elif dm[i]['d'] == d:
-#             s += "," + str(dm[i]['val']) + "]"
-#         d = dm[i]['d']
-#         i += 1
-#     return s
-
 def build_tree(max_depth):
     if max_depth == 0:
-        return {'val': None}
-    t = {'l': build_tree(max_depth - 1), 'r': build_tree(max_depth - 1), 'val': None}
-    if max_depth >= 2:
+        return {'val': None, 'children_full': False}
+    t = {'l': build_tree(max_depth - 1), 'r': build_tree(max_depth - 1), 'val': None, 'children_full': False}
+    if max_depth >= 1:
         t['l']['p'] = t
         t['r']['p'] = t
     return t
 
 
+def notify_parent(node):
+    if 'p' in node and node['p'] is not None \
+            and node['p']['l']['children_full'] and node['p']['r']['children_full']:
+        node['p']['children_full'] = True
+        notify_parent(node['p'])
+
+
 def find_next_free_node_at_depth_d(t, d):
+    print(d)
     if d == 1:
-        if t['l']['val'] is None:
+        if t['val'] is not None:
+            return None
+        elif t['l']['val'] is None:
+            notify_parent(t['r'])
             return t['l']
         elif t['r']['val'] is None:
+            notify_parent(t['r'])
             return t['r']
         else:
             return None
     else:
-        node = find_next_free_node_at_depth_d(t['l'], d-1)
-        if node is None:
+        node = find_next_free_node_at_depth_d(t['l'], d - 1)
+        if node is None or node['children_full']:
+            print("r")
             node = find_next_free_node_at_depth_d(t['r'], d - 1)
-            if node is None:
+            if node is None or node['children_full']:
                 return None
             else:
                 return node
         else:
+            print("l")
             return node
 
-def depthmap2arr(dm):
+
+def depthmap2btree(dm):
     max_depth = max([e['d'] for e in dm])
     t = build_tree(max_depth)
     for i in range(len(dm)):
         d = dm[i]['d']
         node = find_next_free_node_at_depth_d(t, d)
         node['val'] = dm[i]['val']
+        node['children_full'] = True
+        notify_parent(node)
+        print_tree(t)
     return t
-        #notify_parents(node)
 
 
-
+def print_tree(t, d=0):
+    print(" " * 2 * d + "-- val:", t['val'], ", cf:", t['children_full'], ", d:", d)
+    if 'l' in t and t['l'] is not None:
+        print_tree(t['l'], d + 1)
+    if 'r' in t and t['r'] is not None:
+        print_tree(t['r'], d + 1)
 
 
 def split(dm):
@@ -196,14 +178,23 @@ def reduce(dm):
     return dm
 
 
+def pretty_print_t(t):
+    s = ""
+    if not 'l' in t:
+        return str(t['val'])
+    if t['l'] is not None:
+        if t['l']['val'] is not None:
+            s += "[" + str(t['l']['val'])
+        else:
+            s += "[" + pretty_print_t(t['l'])
+    if t['r'] is not None:
+        if t['r']['val'] is not None:
+            s += ", " + str(t['r']['val'])
+        else:
+            s += ", " + str(pretty_print_t(t['r']))
+    s += "]"
+    return s
+
 
 if __name__ == "__main__":
-    with open("test_input", 'r') as fd:
-        lines = [line.strip() for line in fd.readlines()]
-    arr0 = eval(lines[0])
-    dm = arr2depthmap(arr0)
-    for i in range(len(lines)):
-        dm1 = arr2depthmap(eval(lines[i]))
-        dm = add(dm, dm1)
-    print(dm)
-    print(depthmap2arr(dm))
+    main()
